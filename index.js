36f2be4861b9ed7bda743589f87aec3b44692489
Jonathan East <jonathan_east@ymail.com>
2020-12-08 16:24:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const [, , , , , indg] = colors
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
-  const { name } = muppets
+  const { name } = muppet
 
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
